76555d6b675225f630c09ac41a97f667e6bf43b6
Shalev Rahamim <shalevr1997@gmail.com>
2020-06-14 18:26:41 +0300

    -

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const restaurant = require('./pizza-restaurant');
 const fs = require('fs');
 
 restaurant.init(2, 3, 1, 2);
-fs.readFile('orders.json', (err, data)=>{
+fs.readFile('src/orders.json', (err, data)=>{
     if(err)
         throw err;
     let orders = JSON.parse(data);
